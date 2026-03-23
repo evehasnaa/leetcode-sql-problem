ef42b43e23cad08107df91d1278ff6ec568a0032
HASNAA AHMED! <131290991+evehasnaa@users.noreply.github.com>
2026-03-23 10:36:27 +0200

    Refactor SQL
    Updated SQL query to find the second highest distinct salary from the Employee table. Added comments for clarity and removed the old query.

--- second-Highest Salary.sql
+++ second-Highest Salary.sql
@@ -1,4 +1,22 @@
+/*Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+*/
+
 -- Write your MySQL query statement below
+```sql
 /*select  max(salary) as SecondHighestSalary
 from Employee
 where salary<(
@@ -13,3 +31,4 @@ select(
     order by salary desc
     limit 1 offset 1
 ) as SecondHighestSalary;
+```sql
